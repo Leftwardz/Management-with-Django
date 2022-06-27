a506330f5b5895b964bc3f0aa34275cd24be6388
Nathan <nathannalvarenga@hotmail.com>
2022-06-26 21:48:51 -0300

    update style home page

--- Client_Registration/views.py
+++ Client_Registration/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Client
 
 # Create your views here.
 def home(request):
-    return render(request, 'Client_Registration/home.html')
+    clients = Client.objects.all()
+    data = {'clients': clients}
+    return render(request, 'Client_Registration/home.html', data)
