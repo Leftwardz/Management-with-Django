009b2276d1aedfa28f79025da7dc52dcd00fc3d0
nathan.alvarenga <nathannalvarenga@hotmail.com>
2022-06-30 13:16:47 -0300

    Added login feature

--- Client_Registration/views.py
+++ Client_Registration/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from .models import Client
+from django.contrib import messages
 
-# Create your views here.
+
 def home(request):
     clients = Client.objects.all()
     data = {'clients': clients}
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home_url')
+
+        else:
+            messages.add_message(request, messages.ERROR, 'Erro no login.')
+
     return render(request, 'Client_Registration/home.html', data)
